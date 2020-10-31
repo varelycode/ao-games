ce271672cabe18c81c61d21b3396791c40a2fbbe
Max Triano <mtriano234@gmail.com>
2020-10-31 15:47:56 -0400

    Starting to add column ordering heuristic

--- python/gameplay.py
+++ python/gameplay.py
@@ -8,8 +8,12 @@ WIDTH = 7
 class Play:
     mask = '' # Bit mask containing positions of non-empty cells
     num_moves_played = 0 # Number of moves played since beginning of game
-    #def __init__(self, other=None):
-    
+    column_ordering = [] # Order to look at columns in, since columns closer to the center are involved in more 4-alignments
+
+    def __init__(self, other=None):
+        for i in range(WIDTH): # Populate column ordering list, starting in center cols and moving outwards
+            self.column_ordering[i] = (WIDTH / 2) + (1 - 2 * (i % 2)) * (i + 1) / 2
+
     def is_column_free(self, col: int) -> bool:
         mask_int = int(self.mask, 2)
         top_mask = self.get_top_mask(col)
@@ -37,6 +41,7 @@ class Play:
         score = -1 * WIDTH * HEIGHT
 
         for c in range(WIDTH):
+            col_to_play = self.column_ordering[c]
             # make deep copy of current board
             # play by placing piece in column c
             # temp_score = __score(temp_board)
