5784d38fce7de3e4bb226fb951759ea1a7e9a191
varelycode <virosas@ucdavis.edu>
2020-10-31 12:40:42 -0700

    Added board storing to bit board

--- python/client.py
+++ python/client.py
@@ -3,20 +3,23 @@
 import sys
 import json
 import socket
+import boards
+import moves
 
 def get_move(player, board):
   # TODO determine valid moves
   # TODO determine best move
-  return {"column": 1}
+
+  return moves.get_best_move()
 
 def prepare_response(move):
   response = '{}\n'.format(json.dumps(move))
   print('sending {!r}'.format(response))
-  return response.encode(encoding='UTF-8')
+  return response.encode(encoding='UTF-8') #return response as bytes-like
 
 if __name__ == "__main__":
-  port = int(sys.argv[1]) if (len(sys.argv) > 1 and sys.argv[1]) else 1337
-  host = sys.argv[2] if (len(sys.argv) > 2 and sys.argv[2]) else socket.gethostname()
+  port = int(sys.argv[1]) if (len(sys.argv) > 1 and sys.argv[1]) else 1337 # default port 1337, add optional port
+  host = sys.argv[2] if (len(sys.argv) > 2 and sys.argv[2]) else socket.gethostname() # default hostname at gethostname or add optional name
 
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   try:
@@ -30,8 +33,9 @@ if __name__ == "__main__":
       board = json_data['board']
       maxTurnTime = json_data['maxTurnTime']
       player = json_data['player']
+      obj = boards.Board()
+      mask, current = obj.get_bit_board([[0,0,0,0,0,0,0],[0,0,0,2,0,0,0],[0,0,1,1,0,0,0],[0,0,2,1,0,0,0],[0,0,2,2,1,0,0],[0,0,2,1,1,2,0]])
       print(player, maxTurnTime, board)
-
       move = get_move(player, board)
       response = prepare_response(move)
       sock.sendall(response)
